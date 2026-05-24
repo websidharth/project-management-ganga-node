2fda75491451cf30c4bc771e15126794a06a88cf
SID <websidharth@gmail.com>
2026-05-24 14:39:19 +0530

    update cors policy

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(cors()); // <--- Add this line BEFORE your routes
 /*
 app.use(cors({
   origin: "*", // Allow all origins (good for development)
-  // origin: ["https://gmr-teal.vercel.app","http://192.168.1.67:3000", "http://localhost:3000","http://localhost:5000"], // Allow specific IPs
+  // origin: ["https://project-management-ganga-ui.vercel.app","http://192.168.1.67:3000", "http://localhost:3000","http://localhost:5000"], // Allow specific IPs
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
