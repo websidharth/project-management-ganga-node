c4907b81e4e12a02ae55cfbb844eb77ff808a615
SID <websidharth@gmail.com>
2026-06-10 00:27:13 +0530

    updated PV

--- src/dtos/product-variant.dto.ts
+++ src/dtos/product-variant.dto.ts
@@ -5,7 +5,6 @@ export interface ProductVariantDto {
   name: string;
   slug?: string | null;
   productId: number;
-  brandNameId?: number | null;
   productAttributeId?: number | null;
   storeCode: string
   attributeId?: number | null;
@@ -15,63 +14,6 @@ export interface ProductVariantDto {
   lowStockThreshold?: number | null;
   images: string[];
   status: Status;
-  displayOrder?: number | null;
   createdAt: Date;
   updatedAt?: Date | null;
-  isDefault: boolean;
-  extraPrice?: number | null;
-  varient?: string | null;
-  size?: string | null;
-  material?: string | null;
-  voltage?: string | null;
-  color?: string | null;
-  extraSku?: string | null;
-}
-
-export interface CreateProductVariantDto {
-  name: string;
-  slug?: string | null;
-  productId: number;
-  brandNameId?: number | null;
-  productAttributeId?: number | null;
-  storeCode: string
-  attributeId?: number | null;
-  cost?: number;
-  Price?: number;
-  stock?: number;
-  lowStockThreshold?: number | null;
-  images?: string[];
-  status?: Status;
-  displayOrder?: number | null;
-  isDefault?: boolean;
-  extraPrice?: number | null;
-  varient?: string | null;
-  size?: string | null;
-  material?: string | null;
-  voltage?: string | null;
-  color?: string | null;
-  extraSku?: string | null;
-}
-
-export interface UpdateProductVariantDto {
-  name?: string;
-  slug?: string | null;
-  brandNameId?: number | null;
-  productAttributeId?: number | null;
-  attributeId?: number | null;
-  cost?: number;
-  Price?: number;
-  stock?: number;
-  lowStockThreshold?: number | null;
-  images?: string[];
-  status?: Status;
-  displayOrder?: number | null;
-  isDefault?: boolean;
-  extraPrice?: number | null;
-  varient?: string | null;
-  size?: string | null;
-  material?: string | null;
-  voltage?: string | null;
-  color?: string | null;
-  extraSku?: string | null;
 }
--- src/models/product-variant.model.ts
+++ src/models/product-variant.model.ts
@@ -2,41 +2,13 @@ import { Status } from '../enum/status.enum';
 
 export interface ProductVariantModel {
   name: string;
-  slug?: string;
+  slug: string | null;
   productId: number;
-  brandNameId?: number;
-  productAttributeId?: number | null; 
-  attributeId?: number | null;
-  cost?: number;
-  Price?: number;
-  stock?: number;
-  lowStockThreshold?: number | null;
-  status?: Status;
-  displayOrder?: number | null;
-  isDefault?: boolean;
-}
-
-export interface UpdateProductVariantModel {
-  name: string;
-  slug?: string | null;
-  productId: number;
-  brandNameId?: number | null;
-  productAttributeId?: number | null;
-  storeCode: string;
-  attributeId?: number | null;
-  cost?: number;
-  Price?: number;
-  stock?: number;
-  lowStockThreshold?: number | null;
-  images?: string[];
-  status?: Status;
-  displayOrder?: number | null;
-  isDefault?: boolean;
-  extraPrice?: number | null;
-  varient?: string | null;
-  size?: string | null;
-  material?: string | null;
-  voltage?: string | null;
-  color?: string | null;
-  extraSku?: string | null;
-}
+  productAttributeId: number | null;  
+  attributeId: number | null;
+  cost: number;
+  Price: number;
+  stock: number;
+  lowStockThreshold: number; 
+  status: Status;
+} 
